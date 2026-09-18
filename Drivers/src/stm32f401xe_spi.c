9e3d6001027a9149ce4298b07ab947a79687c5b6
vidhyamg <deepuvidhya@gmail.com>
2026-09-18 15:59:57 +0530

    Add files via upload

---
+++ Drivers/src/stm32f401xe_spi.c
@@ -0,0 +1,225 @@
+/*
+ * stm32f401xe_spi.c  (corrected)
+ *
+ *  Created on: Sep 2, 2026
+ *      Author: vidhya
+ *
+ */
+#include "stm32f401xe.h"
+#include "stm32f401xe_spi.h"
+#include "stm32f401xe_rcc.h"
+#include<stdio.h>
+
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
+{
+	if(EnorDi == ENABLE)
+	{
+		if(pSPIx == SPI1)
+		{
+			SPI1_PCLK_EN();
+		}else if (pSPIx == SPI2)
+		{
+			SPI2_PCLK_EN();
+		}else if (pSPIx == SPI3)
+		{
+			SPI3_PCLK_EN();
+		}else if (pSPIx == SPI4)
+		{
+			SPI4_PCLK_EN();
+		}
+	}
+	else
+	{
+		if(pSPIx == SPI1)
+		{
+			SPI1_PCLK_DI();
+		}else if (pSPIx == SPI2)
+		{
+			SPI2_PCLK_DI();
+		}else if (pSPIx == SPI3)
+		{
+			SPI3_PCLK_DI();
+		}else if (pSPIx == SPI4)
+		{
+			SPI4_PCLK_DI();
+		}
+	}
+}
+
+void SPI_Init(SPI_Handle_t *pSPIHandle)
+{
+	//peripheral clock enable
+	SPI_PeriClockControl(pSPIHandle->pSPIx, ENABLE);
+
+	//first lets configure the SPI_CR1 register
+	uint32_t tempreg = 0;
+
+	//1. configure the device mode
+	tempreg |= pSPIHandle->SPIConfig.SPI_DeviceMode << SPI_CR1_MSTR ;
+
+	//2. Configure the bus config
+	if(pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_FD)
+	{
+		//bidi mode should be cleared
+		tempreg &= ~( 1 << SPI_CR1_BIDIMODE);
+
+	}else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_HD)
+	{
+		//bidi mode should be set
+		tempreg |= ( 1 << SPI_CR1_BIDIMODE);
+	}else if (pSPIHandle->SPIConfig.SPI_BusConfig == SPI_BUS_CONFIG_SIMPLEX_RXONLY)
+	{
+		//BIDI mode should be cleared
+		tempreg &= ~( 1 << SPI_CR1_BIDIMODE);
+		//RXONLY bit must be set
+		tempreg |= ( 1 << SPI_CR1_RXONLY);
+	}
+
+	// 3. Configure the spi serial clock speed (baud rate)
+	tempreg |= pSPIHandle->SPIConfig.SPI_SclkSpeed << SPI_CR1_BR;
+
+	//4.  Configure the DFF
+	tempreg |= pSPIHandle->SPIConfig.SPI_DFF << SPI_CR1_DFF;
+
+	//5. configure the CPOL
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPOL << SPI_CR1_CPOL;
+
+	//6 . configure the CPHA
+	tempreg |= pSPIHandle->SPIConfig.SPI_CPHA << SPI_CR1_CPHA;
+
+	tempreg |= pSPIHandle->SPIConfig.SPI_SSM << SPI_CR1_SSM;
+
+	pSPIHandle->pSPIx->CR1 = tempreg;
+}
+
+void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
+{
+	if(EnOrDi == ENABLE)
+	{
+		pSPIx->CR1 |=  (1 << SPI_CR1_SPE);
+	}else
+	{
+		pSPIx->CR1 &=  ~(1 << SPI_CR1_SPE);
+	}
+}
+
+void  SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
+{
+	if(EnOrDi == ENABLE)
+	{
+		pSPIx->CR1 |=  (1 << SPI_CR1_SSI);
+	}else
+	{
+		pSPIx->CR1 &=  ~(1 << SPI_CR1_SSI);
+	}
+}
+
+void  SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi)
+{
+	if(EnOrDi == ENABLE)
+	{
+		pSPIx->CR2 |=  (1 << SPI_CR2_SSOE);
+	}else
+	{
+		pSPIx->CR2 &=  ~(1 << SPI_CR2_SSOE);
+	}
+}
+
+void SPI_DeInit(SPI_RegDef_t *pSPIx)
+{
+	if (pSPIx == SPI1)
+	{
+		SPI1_REG_RESET();
+	}
+	else  if (pSPIx == SPI2)
+	{
+		SPI2_REG_RESET();
+	}
+	else  if (pSPIx == SPI3)
+	{
+		SPI3_REG_RESET();
+	}
+	else  if (pSPIx == SPI4)
+	{
+		SPI4_REG_RESET();
+	}
+}
+
+// Get flag status from SR register
+uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx , uint32_t FlagName)
+{
+	if(pSPIx->SR & FlagName)
+	{
+		return FLAG_SET;
+	}
+	return FLAG_RESET;
+}
+
+/*
+ * SPI_SendData - blocking call
+ */
+void SPI_SendData(SPI_RegDef_t *pSPIx,uint8_t *pTxBuffer, uint32_t Len)
+{
+	while(Len > 0)
+	{
+		//1. wait until TXE is set
+		while(SPI_GetFlagStatus(pSPIx,SPI_TXE_FLAG)  == FLAG_RESET );
+
+		//2. check the DFF bit in CR1
+		if( (pSPIx->CR1 & ( 1 << SPI_CR1_DFF) ) )
+		{
+			//16 bit DFF
+			//1. load the data in to the DR
+			pSPIx->DR =   *((uint16_t*)pTxBuffer);
+			Len--;
+			Len--;
+			pTxBuffer += 2;   // advance by 2 bytes
+		}else
+		{
+			//8 bit DFF
+			pSPIx->DR =   *pTxBuffer;
+			Len--;
+			pTxBuffer++;
+		}
+	}
+}
+
+/*
+ * SPI_ReceiveData - blocking call
+ */
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len)
+{
+	while(Len > 0)
+	{
+		//1. wait until RXNE is set
+		while(SPI_GetFlagStatus(pSPIx,SPI_RXNE_FLAG)  == (uint8_t)FLAG_RESET );
+
+		//2. check the DFF bit in CR1
+		if( (pSPIx->CR1 & ( 1 << SPI_CR1_DFF) ) )
+		{
+			//16 bit DFF
+			//1. load the data from DR to Rxbuffer address
+			*((uint16_t*)pRxBuffer) = pSPIx->DR ;
+			Len--;
+			Len--;
+			pRxBuffer += 2;   // advance by 2 bytes
+		}else
+		{
+			//8 bit DFF
+			*(pRxBuffer) = pSPIx->DR ;
+			Len--;
+			pRxBuffer++;
+		}
+	}
+}
+
+/*
+ * SPI_ClearOVRFlag - clears the Overrun flag by reading DR then SR
+ */
+void SPI_ClearOVRFlag(SPI_RegDef_t *pSPIx)
+{
+	uint8_t temp;
+	temp = pSPIx->DR;
+	temp = pSPIx->SR;
+	(void)temp;
+}
